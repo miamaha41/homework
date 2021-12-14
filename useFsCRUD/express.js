6d8262e4986ea5c218437d3ece793f2fc3405c0a
Nguyen Thi Hai Yen <miamaha41@gmail.com>
2021-12-14 17:23:03 +0700

    fix npm init

--- useFsCRUD/express.js
+++ useFsCRUD/express.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
-app.get('/tin-tuc', (req, res) => {
+app.use(morgan("dev"));
+app.get('/', (req, res) => {
     res.send("Hello world!")
 })
 app.listen(port, () => {
-   
     console.log(`Example app listening at http://localhost:${port}`)
   })
