19d41ec4253d23dd31f818b80a4f5f800de844dc
Nguyen Thi Hai Yen <miamaha41@gmail.com>
2021-12-15 19:43:59 +0700

    delete node_modules

--- useFsCRUD/express.js
+++ useFsCRUD/express.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const userRouter = require('./user')
 const app = express();
 const port = 3000;
 app.use(morgan("dev"));
@@ -9,3 +10,10 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
   })
+
+const middleware = (req, res, next) => {
+    req.real_name = "vinh";
+    console.log('req')
+    next();
+}
+app.use('/user', middleware, middleware, middleware, userRouter)
